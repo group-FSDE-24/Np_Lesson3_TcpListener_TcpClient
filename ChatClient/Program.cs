de8b52d748884f0760d3730328b76a1d017701f4
karimzade_k <karimzade_k@STHQ0118-01>
2025-10-05 20:28:22 +0400

    ChatClient completed

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace ChatClient;
+﻿using System.Net.Sockets;
+
+namespace ChatClient;
 
 
 class Program
 {
     static void Main()
     {
-        
+        var client = new TcpClient("10.1.18.1", 27001);
+
+        var serverStream = client.GetStream();
+
+        var br = new BinaryReader(serverStream);
+        var bw = new BinaryWriter(serverStream);
+
+
+
+        Task.Run(() =>
+        {
+            while (true)
+            {
+                Console.WriteLine(br.ReadString());
+            }
+        });
+
+
+
+        while (true)
+        {
+            bw.Write(Console.ReadLine() ?? "Okey");
+        }
+
+
+
+
+
+
     }
 }
