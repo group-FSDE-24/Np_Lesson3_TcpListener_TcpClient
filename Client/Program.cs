5693a5bde23920c70407ccb38d3ef5c59982f0fb
karimzade_k <karimzade_k@STHQ0118-01>
2025-10-05 18:51:32 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Client;
+
+
+class Program
+{
+    static void Main()
+    {
+        var client = new TcpClient("127.0.0.1", 27001);
+
+        var stream = client.GetStream();
+
+        var sw = new StreamWriter(stream);
+        var sr = new StreamReader(stream);
+
+
+        while (true)
+        {
+            Console.WriteLine("Client to Message....");
+            sw.Write(Console.ReadLine());
+            Console.WriteLine($"Server answer: {sr.ReadToEnd()}");
+        }
+
+
+
+
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server;
+
+class Program
+{
+    static void Main()
+    {
+        var ip = IPAddress.Parse("127.0.0.1");
+        var port = 27001;
+
+        TcpListener tcpListener = new TcpListener(ip, port);
+
+        tcpListener.Start(10);
+
+        Console.WriteLine($"{tcpListener.Server.LocalEndPoint} listening.....");
+
+        var client = tcpListener.AcceptTcpClient();
+
+        Console.WriteLine($"{client.Client.RemoteEndPoint} connected server....");
+
+
+        var stream = client.GetStream();
+
+        var sr = new StreamReader(stream);
+        var sw = new StreamWriter(stream);
+
+
+        while (true)
+        {
+            Console.WriteLine(sr.ReadToEnd());
+            sw.Write("Good Message");
+        }
+
+    }
+}
